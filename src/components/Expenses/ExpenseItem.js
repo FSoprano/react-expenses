202ea3cde139e20a40be5febb6af4857554f71aa
KUESTER <kuester@de.ibm.com>
2021-10-11 18:40:20 +0200

    Finish lesson 48

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,24 @@
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 const ExpenseItem = (props) => {
+
+  const [title, setTitle] = useState(props.ttle)
   
+  const clickHandler = () => {
+    setTitle('Updated');
+    console.log(title)
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate dte = {props.dte}/>
       
       <div className="expense-item__description">
-        <h2>{props.ttle}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amt}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };

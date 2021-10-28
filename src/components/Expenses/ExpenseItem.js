c98e0c61c8b789b9d2ac182bbe236f866dd5c291
KUESTER <kuester@de.ibm.com>
2021-10-28 21:19:27 +0200

    Finish section 4 (including filterChangeHandler assignment)

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,10 +1,14 @@
-import React, { useState } from 'react';
+// mport React, { useState } from 'react';
+// useState import removed because 'Change Title' button was removed.
+// Now a stateless, presentational or dumb component
+import React from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 const ExpenseItem = (props) => {
 
-  const [title, setTitle] = useState(props.ttle)
+  // const [title, setTitle] = useState(props.ttle)
+  // --> useState call for 'Change Title' button. Removed. See below.
   // Why does const work here? It's because we never assign a new value with the 
   // equals operator. We call a value-changing function. React does the 
   // value changing. This is why const works fine. React does not re-initialze the value 
@@ -30,30 +34,36 @@ const ExpenseItem = (props) => {
   // again. This is exactly what we need.
   // useState is a React hook.
   
-  const clickHandler = () => {
+  /* const clickHandler = () => {
     // Why does 'let title = 'Updated'' not work?
     // Answer: the value would be updated, as can be seen on the console, 
     // but there is nothing that would re-execute this component function, which 
     // is required to change what we see on the page. This is where React state comes in.
     // Calling the useState function setTitle will cause a new execution of the 
     // component function.
+    // Removed setTitle (Was for 'Change Title' button)
     setTitle('Updated');
     console.log(title)
     // You still see the old value (toilet paper) on the console. That's because React doesn't make 
     // the change immediately; it schedules it.
   }
+  */
   return (
     <Card className="expense-item">
       <ExpenseDate dte = {props.dte}/>
       
       <div className="expense-item__description">
-        <h2>{title}</h2> {/* Mind that it must be {title} now, 
+        <h2>{props.title}</h2> 
+        {/* Mind that as long as the 'Change Title' button was still 
+        active, it had to be {title} now, 
         the first value in the array returned by the useState call. 
         If props.ttle remains, we will not see the change.*/}
         <div className="expense-item__price">${props.amt}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
-      {/* onClick adds an event handler. Very easy in React.
+      
+      {/* <button onClick={clickHandler}>Change Title</button>
+      --> Removed. Was just there for demo purposes
+      onClick adds an event handler. Very easy in React.
       clickHandler is just a pointer. If we'd write clickHandler() here, 
       the function would get executed when the JSX code is executed, and not when 
       the button is clicked. That would be too early*/}

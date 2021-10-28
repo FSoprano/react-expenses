7eba6ad8d95487dc64921c45d43f06124ce03e99
KUESTER <kuester@de.ibm.com>
2021-10-28 18:54:17 +0200

    Moved exp object to App.js, used props in Expenses. Continue with lesson 61

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,49 +1,29 @@
 import ExpenseItem from "./ExpenseItem";
 import Card from '../UI/Card';
 import "./Expenses.css";
-const Expenses = () => {
-    const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+const Expenses = (props) => {
+    
   return (
     <Card className="expenses">
       <ExpenseItem
-        ttle={expenses[0].title}
-        amt={expenses[0].amount}
-        dte={expenses[0].date}
+        ttle={props.items[0].title}
+        amt={props.items[0].amount}
+        dte={props.items[0].date}
       ></ExpenseItem>
       <ExpenseItem
-        ttle={expenses[1].title}
-        amt={expenses[1].amount}
-        dte={expenses[1].date}
+        ttle={props.items[1].title}
+        amt={props.items[1].amount}
+        dte={props.items[1].date}
       ></ExpenseItem>
       <ExpenseItem
-        ttle={expenses[2].title}
-        amt={expenses[2].amount}
-        dte={expenses[2].date}
+        ttle={props.items[2].title}
+        amt={props.items[2].amount}
+        dte={props.items[2].date}
       ></ExpenseItem>
       <ExpenseItem
-        ttle={expenses[3].title}
-        amt={expenses[3].amount}
-        dte={expenses[3].date}
+        ttle={props.items[3].title}
+        amt={props.items[3].amount}
+        dte={props.items[3].date}
       ></ExpenseItem>
     </Card>
   );
